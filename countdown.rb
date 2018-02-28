1fbae8c91bf133a4fe5cb06ac0686ab7b0416306
Aspen James <aspenjames@tqca.org>
2018-02-28 22:40:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(s)
+  while s > 0
+    puts "#{s} SECONDS!" unless s == 1
+    puts "1 SECOND!" if s == 1
+    s -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
